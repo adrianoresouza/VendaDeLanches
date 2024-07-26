6bbe9f9b4d824baca37e712681b999eba583ee53
adrianoresouza <adrianoresouza@gmail.com>
2024-07-26 08:01:04 -0300

    Novo navbar

--- obj/Debug/net8.0/LanchesMac.AssemblyInfo.cs
+++ obj/Debug/net8.0/LanchesMac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6522ab4264e98dddc547c3b29849848dcacd895d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
